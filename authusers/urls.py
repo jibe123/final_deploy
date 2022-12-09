67904611246191ac75814a437bf47b3d8364f723
jibe123 <jibek.k@gmail.com>
2022-12-09 07:37:55 +0600

    initial commit

--- authusers/urls.py
+++ authusers/urls.py
@@ -11,17 +11,17 @@ import authusers.views as vs
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(),
-         name='token_refresh'),
-    path('password_reset/', include(
-        'django_rest_passwordreset.urls',
-        namespace='password_reset')),
+    # path('token/refresh/', TokenRefreshView.as_view(),
+    #      name='token_refresh'),
+    # path('password_reset/', include(
+    #     'django_rest_passwordreset.urls',
+    #     namespace='password_reset')),
     path('change_password/', vs.ChangePasswordView.as_view(),
          name='change-password'),
     path('change_profile_photo/', vs.ChangeProfilePhotoView.as_view(),
          name='change-profile-photo'),
-    # path('login/', LoginView.as_view(
-    #     template_name='admin/login.html'), name='login'),
+    path('login/', LoginView.as_view(
+        template_name='admin/login.html'), name='login'),
     # path('logout/', LogoutView.as_view(
     #     template_name='admin/logout.html'), name='logout'),
 ]
