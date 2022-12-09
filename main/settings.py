aad0840450f2d698adb800147e8d9b886f1f8b0c
jibe123 <jibek.k@gmail.com>
2022-12-09 07:47:52 +0600

    initial commit

--- main/settings.py
+++ main/settings.py
@@ -137,10 +137,10 @@ TIME_INPUT_FORMATS = ('%H:%M',)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'accounts.User'
